1c433efd453f037d914f49239f1d61383e0abebc
Matheus de Almeida Cantarutti <81988636+cantaruttim@users.noreply.github.com>
2023-05-11 22:00:57 -0300

    Add files via upload

--- IBGE/geobr.R
+++ IBGE/geobr.R
@@ -25,7 +25,9 @@ ggplot() + geom_sf(data = meso, fill = "#2D3E50",
                    color = "#FEBF57", size = .15,
                    show.legend = F)
 
-sp = read_state(code_state = "SP", year = 2020)
+sp = read_state(code_state = "SP")
+rj = read_state(code_state = "RJ")
+
 ggplot() + geom_sf(data = sp, fill = "#2D3E50",
                    color = "#FEBF57", size = .15,
                    show.legend = F)
@@ -41,3 +43,22 @@ ggplot() + geom_sf(data = meso_sp, fill = "#2D3E50",
                    show.legend = F)
 
 plot(micro_reg_sp)
+
+municipios <- read_municipality(code_muni = "SP")
+ggplot() + geom_sf(data = municipios, fill = "#2D3E50",
+                   color = "#FEBF57", size = .15,
+                   show.legend = F)
+View(municipios)
+
+municipios_rj <- read_municipality(code_muni = "RJ")
+
+
+## SP + RJ
+municipios <- read_municipality(code_muni = "SP")
+ggplot() + geom_sf(data = municipios, fill = "#2D3E50",
+                   color = "#FEBF57", size = .15,
+                   show.legend = F) + geom_sf(data = municipios_rj, fill = "#2D3E50",
+          color = "#FEBF57", size = .15,
+          show.legend = F)
+  
+  
